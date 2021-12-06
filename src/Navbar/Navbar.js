55020a7746e6e56e0a647b38f693b426c5a8d4b2
jyotip101 <jyotipatelfyit77100@gmail.com>
2021-12-06 11:53:51 +0530

    navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,49 +1,59 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Navbar.css'
+import { nav, sociallIcon } from './navData'
 
 const Navbar = () => {
+  const [showLink, setShowLink] = useState(false)
+  const linkContanerRef = useRef(null)
+  const linkRef = useRef(null)
+
+  useEffect(() => {
+    const linkHeight = linkRef.current.getBoundingClientRect().height
+    if (showLink) {
+      linkContanerRef.current.style.height = `${linkHeight}px`
+    } else {
+      linkContanerRef.current.style.height = '0px'
+    }
+  }, [showLink])
+
   return (
     <>
       <nav>
         <div className='nav-center'>
           <div className='nav-header'>
-            <span class='material-icons'>logo_dev</span>
-            <button className='menu-button'>
-              <i className='material-icons'>drag_handle</i>
+            <span className='material-icons'>logo_dev</span>
+            <button
+              className='menu-button'
+              onClick={() => setShowLink(!showLink)}
+            >
+              <i className='material-icons'>{`${
+                showLink ? 'close' : 'drag_handle'
+              }`}</i>
             </button>
           </div>
-          <div className='links-container'>
-            <ul className='links'>
-              <li>
-                <a href='#'>Home</a>
-              </li>
-              <li>
-                <a href='#'>About</a>
-              </li>
-              <li>
-                <a href='#'>Contact</a>
-              </li>
-              <li>
-                <a href='#'>Products</a>
-              </li>
+          <div className='links-container' ref={linkContanerRef}>
+            <ul className='links' ref={linkRef}>
+              {nav.map((item) => {
+                const { id, text, url } = item
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                )
+              })}
             </ul>
           </div>
           <ul className='social-icons'>
-            <li>
-              <a href='https://twitter.com'>
-                <i className='material-icons'>insert_emoticon</i>
-              </a>
-            </li>
-            <li>
-              <a href='https://twitter.com'>
-                <i className='material-icons'>insert_emoticon</i>
-              </a>
-            </li>
-            <li>
-              <a href='https://twitter.com'>
-                <i className='material-icons'>insert_emoticon</i>
-              </a>
-            </li>
+            {sociallIcon.map((item) => {
+              const { id, url, icon } = item
+              return (
+                <li key={id}>
+                  <a href={url}>
+                    <i className='material-icons'>{icon}</i>
+                  </a>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </nav>
---
+++ src/Navbar/navData.js
@@ -0,0 +1,44 @@
+export const nav = [
+  {
+    id: 1,
+    url: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '/',
+    text: 'About',
+  },
+  {
+    id: 3,
+    url: '/',
+    text: 'Contact',
+  },
+  {
+    id: 4,
+    url: '/',
+    text: 'Products',
+  },
+]
+export const sociallIcon = [
+  {
+    id: 1,
+    url: 'https://twitter.com',
+    icon: 'insert_emoticon',
+  },
+  {
+    id: 2,
+    url: 'https://twitter.com',
+    icon: 'insert_emoticon',
+  },
+  {
+    id: 3,
+    url: 'https://twitter.com',
+    icon: 'insert_emoticon',
+  },
+  {
+    id: 4,
+    url: 'https://twitter.com',
+    icon: 'insert_emoticon',
+  },
+]
